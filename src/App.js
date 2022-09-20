1f7ac15f2d921c01d808864bb9971598026a7ad5
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-09-20 07:01:19 +0300

    react multi-search funtion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import "./app.css";
+import "./App.css";
 import { Users } from "./Users";
 import { useState } from "react";
+import Table from "./Table";
 
 function App() {
   const [query, setQuery] = useState("");
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
 
   return (
     <div className="app">
@@ -13,15 +22,7 @@ function App() {
         className="search"
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLowerCase().includes(query)
-        ).map((user) => (
-          <li key={user.id} className="listItem">
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+      <Table data={search(Users)} />
     </div>
   );
 }
